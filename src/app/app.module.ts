dc16efad0799754aa89ff65f996cdb03661ea05e
shailja123 <shailja@nisostech.com>
2019-09-17 17:30:50 +0530

    created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HighlightComponent } from './highlight/highlight.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HighlightComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
