ce04405460149f5cec82f2a5c36ba489194f97f1
shailja123 <shailja@nisostech.com>
2019-09-18 14:58:39 +0530

    ng-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   isEditable: boolean;
   inputField: string;
   progreessValue: number;
+  ngContentList = [{ 'value': 'a', 'isTrue': true}, { 'value': 'b', 'isTrue': false }, { 'value': 'c', 'isTrue': true }, { 'value': 'd', 'isTrue': false }, { 'value': 'e', 'isTrue': false }, { 'value': 'f', 'isTrue': true }];
+  showValue = true;
 
   startProgress() {
     for (let i = 0; i <= 5; i++) {
